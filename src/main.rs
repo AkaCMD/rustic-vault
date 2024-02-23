30fda195bfc0fbc04b10d507f1df2fefb12975aa
Minda Chen <akacmd333@gmail.com>
2024-02-23 22:17:44 +0800

    Added GDT to prevent triple fault

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ pub extern "C" fn _start() -> ! {
     println!("Hello Sailor{}", "!");
 
     rustic_vault::init();
+    fn stack_overflow() {
+        stack_overflow();
+    }
+    stack_overflow();
 
     // invoke a breakpoint exception
     x86_64::instructions::interrupts::int3();
